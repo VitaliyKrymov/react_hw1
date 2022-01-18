5cb7b24324f8d1796c0722be3e64b55631a71fe5
VitaliiKrymov <krymvitaliy@gmail.com>
2022-01-18 23:46:19 +0200

    users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,15 @@
+import {useEffect, useState} from "react";
 
 
 const Users = () => {
-    let users = [];
-    fetch("https://jsonplaceholder.typicode.com/users")
-        .then(value => value.json())
-        .then(value =>users=value)
+
+    const [users,setUsers]= useState([],);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(value => value.json())
+            .then(value => setUsers(value))
+    },[])
+
     return (
         <div>
             {users.map(value => JSON.stringify(value))}
