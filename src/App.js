feee0adde3cb06be46c625d5efc77fb6b4ee92ec
VitaliiKrymov <krymvitaliy@gmail.com>
2022-01-23 18:04:44 +0200

    base knowlege for react constructor form fetch

--- src/App.js
+++ src/App.js
@@ -1,140 +1,92 @@
+import React, {Component} from "react";
+
 import './App.css';
 
-const name = 'Demo video'
-const part = 2;
-const favoriteMovie = `Star ${part}`;
-let variable = 'Chanal 25'
 
-const fruites = ['banana', 'orange', 'apple'];
-fruites.push('mango');
-const [banana, orange, ...restFruites] = fruites;
-
-// function sum(a, b) {              //function declaration
-//     console.log(a, b);
-//     console.log(a+ b);
-//     return a + b;
-// }
-const multiple = function (a,b)  {   //function expresion
-  return a*b;
-}
-console.log(multiple(2, 20));
-
-const sum2 = (...rest) => {
-    let sum=0;
-    for (let i=0;i<rest.length;i++){
-        sum +=rest[i]
+class User {
+    constructor(userName, userAge, fullName) {
+        this.name = userName;
+        this.age = userAge;
+        this.fullName = fullName;
     }
-    return sum
-};        //arrow function
-console.log('Sum: ',sum2(2,3,4,5,6))
 
-const myFruits = (fruites= []) => {
-    let html = [];
-
-    // try {
-        for (let i = 0; i < fruites.length; i++) {
-            console.log(i, fruites[i]);
-            html.push(<p>{i}-{fruites[i]}</p>);//пушим параграфи в масив html
-        }
-    // } catch (e){
-    //     console.error('New error:',e);
-    //     html.push(<h1>No fruits</h1>)
-    // }
-
-    return html;
-}
-
-const user = {
-    name: 'Max',
-    lastName: 'Rudniy',
-    age: 27,
-    status: false,
-    fruites,
-    gender: 'male',
-    bestFriend: {
-        name: "Nataly",
-        age: 43
+    getName() {
+        console.log(this.name)
     }
-};
 
-const user2 = {
-    ...user,
-    bestFriend: {
-        ...user.bestFriend
+    sayHello() {
+        console.log('Hello I am ', this.age, 'years old.')
     }
 }
-user2.name = 'Vitaliy'
-user2.lastName = 'Krymov'
-user2.bestFriend.name = 'Natasha'
 
-console.log(user);
-console.log(user2);
+class Admin extends User {
+    constructor(name, age, fullName, rights) {
+        super(name, age, fullName);
+        this.rights = rights;
+    }
 
-const {name: nameUser, age, lastName, status, city = "Kyiv", ...rest} = user;
+    getName() {
+        console.log('I am admin. My name is: ', this.name);
+    }
+}
 
-const iLikeTheseFruits=myFruits(restFruites);
+const vitaliy = new User('Vitaliy', 27, 'Vitaliy Krymov');
+const admin = new Admin('New Admin', 27, "No full name", 'write');
+console.log(admin);
+admin.getName();
+admin.sayHello();
 
-const User = ({name="Guest"}) => (<h1>{name}</h1> || <p>No Data</p>);
+console.log(vitaliy);
+
+vitaliy.getName();
 
 
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            firstName: '',
+            email: ''
+        };
+        // this.onClickHandler=this.onClickHandler.bind(this)//arrow function make it useless
+    }
 
-// function User({name, lastName,gender, ...rest}) {
-// return (name && <h1>{name}</h1>)||(<h2>{"N0 name"}</h2>)//якщо є нейм тоді виведемо нейм інакше"N0 name"
-
-    // if (name && lastName) {
-//     if (name ||lastName) {
-//         const salut=`Hello,${gender ==="male"?'Mr':'Ms'} ${name}`;
-//         return name ? (<h1>{salut}</h1>): (<p>No Data</p>)// ternarka
-//     }
-// return null;
-    // if (!name) {
-    //     return null;
+    // onClickHandler=()=>{
+    //     console.log('onClick',this);
+    //     this.setState({
+    //         firstName:'New User Name'
+    //     })
     // }
-    // if (name) {
-    //     return (
-    //         <div>
-    //             <h1>{name}</h1>
-    //             <h2>{lastName}</h2>
-    //             <p>{JSON.stringify(rest)}</p>
-    //         </div>
-    //     );
-    // } else {
-    //     return <h2>No Data</h2>;
-    // }
-    // return name ?"true": 'false'// ternarka
-// }
 
+    onSubmite = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+       debugger
+    }
+    onChange = (e) => {
+        const {value, name} = e.target;
 
-function App() {
-    return (
-        <div className="App">
+        this.setState({
+            [name]: value
+        });
+    }
 
-            <User name={user.name}/>
-            {myFruits()}
-            {/*{iLikeTheseFruits}*/}
-            {/*{sum(5,10)}*/}
-            <br/>
-            {/*{multiple(3,3)}*/}
-            <br/>
-            {/*{sum2(5,50,500,1000)}*/}
+    render() {
+        console.log(this.state);
+        return <div className="App">
+            Hello ,{this.state.firstName}!
+            <form onSubmit={this.onSubmite}>
+                <input value={this.state.firstName} onChange={this.onChange} name='firstName' type='text'
+                       placeholder='Put your name'/>
+                <input value={this.state.email} onChange={this.onChange} name='email' type='text'
+                       placeholder='Put your email'/>
+                <button>Submite</button>
 
-            {/*{html}*/}
-            {/*<h1>{nameUser}-{lastName} age:{age} {city}</h1>*/}
-            {/*{JSON.stringify(restFruits)}*/}
-            {/*<h2>{JSON.stringify(rest)}</h2>*/}
-            {/*<div>{restFruits[0]}</div>*/}
-            {/*<div>{restFruits[1]}</div>*/}
-            {/*<div> Fruit: {fruits[3]}</div>*/}
-            {/*{fruits.map(function (fruit, index) {*/}
-            {/*    return <div key={fruit}>fruit{index}:{fruit}</div>*/}
-            {/*})}*/}
-            {/*<div>First fruit:{banana}</div>*/}
-            {/*<div>Second fruit:{orange}</div>*/}
-
-            {/*<User {...user}/>*/}
-            {/*<User/>*/}
-
+            </form>
         </div>
-    );
+    }
+
+
 }
+
 export default App;
