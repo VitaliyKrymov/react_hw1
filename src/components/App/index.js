ce59439c5977d3236b74219f7266022eac86dd71
VitaliiKrymov <krymvitaliy@gmail.com>
2022-01-25 00:10:38 +0200

    start 1 03

---
+++ src/components/App/index.js
@@ -0,0 +1,29 @@
+import {Component} from "react";
+
+import User from "../User";
+
+class App extends Component {
+constructor() {
+    super();
+    this.state={
+        name: 'Vitaliy',
+        date: Date.now(),
+        age: 48
+    }
+}
+    render() {
+        return <div>
+        <User name={this.state.name}
+              age={this.state.age}
+              date={this.state.date}/>
+        <User name='Nataly' age='43'/>
+
+    </div>
+    }
+
+
+
+
+}
+
+export default App;
---
+++ src/components/User/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const User = (props) => {
+
+    return(
+        <div>
+            <p>Name:{props.name}</p>
+            <p>Age:{props.age}</p>
+            <p>It's {props.date}</p>
+
+        </div>
+    )
+};
+
+export default User;
