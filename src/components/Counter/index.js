ed095289f55b48a8c5ad10c057c7759e460b532b
VitaliiKrymov <krymvitaliy@gmail.com>
2022-02-03 21:57:52 +0200

    part 2

---
+++ src/components/Counter/index.js
@@ -0,0 +1,32 @@
+import {useState} from "react";
+
+const Counter = () => {
+    const [clicks,setClicks]=useState(0);
+    const [step,setStep]=useState(1);
+
+    const showValue=()=>{
+        setTimeout(()=>{
+            alert(clicks);
+        },3000)
+    }
+
+  return (
+      <div>
+          <div> Counter:{clicks}</div>
+          <div> Step:{step}</div>
+
+          <button onClick={()=>setClicks(clicks+step)}> Counter +{step}</button>
+          <br/>
+          <button onClick={showValue}> Show value</button>
+          <br/>
+          <input name='step'
+                 value={step}
+                 onChange={(e)=>setStep(+e.target.value)}
+
+              />console.log(e);
+
+      </div>
+  )
+};
+
+export default Counter;
