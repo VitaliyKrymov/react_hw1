a70ad4074d832b6fd646bbaa3052843946e6ea78
VitaliiKrymov <krymvitaliy@gmail.com>
2022-02-06 23:10:02 +0200

    start hooks 1 10  sort posts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './components/posts/PostItem.jsx'
 
 import PostList from "./components/postList/PostList";
 import PostForm from "./components/UI/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -11,19 +12,36 @@ function App() {
         {id: 2, title: "JavaScript 2", body: 'Description'},
         {id: 3, title: "JavaScript 3", body: 'Description'}
     ]);
-
+    const [selectedSort, setSelectedSort] = useState('');
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+    const sortPosts = (sort) => {
+      setSelectedSort(sort);
+     //todo 1:10:51
+    }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
+            <hr style={{margin:'15px'}}/>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts()}
+                    defaultValue='Сортировка по'
+                    options={[
+                        {value:'title', name: 'По названию'},
+                        {value:'body', name: 'По описанию'}
+                    ]}
+                />
 
-            {posts.length !== 0
+            </div>
+
+            {posts.length
                 ? <PostList remove={removePost} posts={posts} title='JS posts 1'/>
                 : <h2 style={{textAlign: 'center'}}>
                     'Посты не найдены'
